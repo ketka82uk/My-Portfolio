d9f2c76c5864b8b387eb1ab04dce7b3249ebe6ff
Cathy <cathy_thomas_uk@yahoo.com>
2021-03-27 18:36:12 +0000

    27Mar1836

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,146 @@
-import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import React from 'react';
+import ReactFullpage from "@fullpage/react-fullpage";
 
-export default function Home() {
-  return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
-  )
+const SEL = 'custom-section';
+const SECTION_SEL = `.${SEL}`;
+
+// NOTE: if using fullpage extensions/plugins put them here and pass it as props.
+// This is no longer required for the scrollOverflow option.
+const pluginWrapper = () => {
+  /*
+  * require('./fullpage.fadingEffect.min'); // Optional. Required when using the "fadingEffect" extension.
+  */
+};
+
+const originalColors = ['#ff5f45', '#0798ec', '#fc6c7c', '#435b71', 'orange', 'blue', 'purple', 'yellow'];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sectionsColor: [...originalColors],
+      fullpages: [
+        {
+          text: 'Section 1',
+        },
+        {
+          text: 'Section 2',
+        },
+        {
+          text: 'Section 3',
+        }
+      ],
+    };
+  }
+
+  onLeave(origin, destination, direction) {
+    console.log('onLeave', { origin, destination, direction });
+    // arguments are mapped in order of fullpage.js callback arguments do something
+    // with the event
+  }
+
+  handleChangeColors() {
+    const newColors =
+      this.state.sectionsColor[0] === 'yellow'
+        ? [...originalColors]
+        : ['yellow', 'blue', 'white'];
+    this.setState({
+      sectionsColor: newColors,
+    });
+  }
+
+  handleAddSection() {
+    this.setState(state => {
+      const { fullpages } = state;
+      const { length } = fullpages;
+      fullpages.push({
+        text: `section ${length + 1}`,
+        id: Math.random(),
+      });
+
+      return {
+        fullpages: [...fullpages],
+      };
+    });
+  }
+
+  handleRemoveSection() {
+    this.setState(state => {
+      const { fullpages } = state;
+      const newPages = [...fullpages];
+      newPages.pop();
+
+      return { fullpages: newPages };
+    });
+  }
+
+  moveSectionDown() {
+    window.fullpage_api.moveSectionDown();
+  }
+
+  render() {
+    const { fullpages } = this.state;
+
+    if (!fullpages.length) {
+      return null;
+    }
+
+    const Menu = () => (
+      <div
+        className="menu"
+        style={{
+          position: 'fixed',
+          top: 0,
+          zIndex: 100,
+        }}
+      >
+        <ul class="actions">
+          <li>
+            <button onClick={() => this.handleAddSection()}>Add Section</button>
+            <button onClick={() => this.handleRemoveSection()}>
+              Remove Section
+            </button>
+            <button onClick={() => this.handleChangeColors()}>
+              Change background colors
+            </button>
+            <button onClick={() => this.moveSectionDown()}>
+              Move Section Down
+            </button>
+          </li>
+        </ul>
+      </div>
+    );
+
+    return (
+      <div className="App">
+        <Menu />
+        <ReactFullpage
+          debug /* Debug logging */
+
+          // Required when using extensions
+          pluginWrapper={pluginWrapper}
+
+          // fullpage options
+          licenseKey={'4AEC80C9-18894042-B6E03B43-F648E604'} // Get one from https://alvarotrigo.com/fullPage/pricing/
+          navigation
+          anchors={['firstPage', 'secondPage', 'thirdPage']}
+          sectionSelector={SECTION_SEL}
+          onLeave={this.onLeave.bind(this)}
+          sectionsColor={this.state.sectionsColor}
+
+          render={comp => (
+            <ReactFullpage.Wrapper>
+              {fullpages.map(({ text }) => (
+                <div key={text} className={SEL}>
+                  <h1>{text}</h1>
+                </div>
+              ))}
+            </ReactFullpage.Wrapper>
+          )}
+        />
+      </div>
+    );
+  }
 }
+
+export default App;
