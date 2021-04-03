35faab3b39b25ccd6b2da80512c59401c3714be2
Cathy <cathy_thomas_uk@yahoo.com>
2021-04-03 19:25:49 +0100

    3Apr1925

---
+++ src/components/projectOne.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function ProjectOne() {
+  return <div>
+    <h3>Tic-Tac-Dino</h3>
+    <p>A week-long individual project to create a grid-based Ultimate Tic-Tac-Toe game using vanilla JavaScript and dinosaurs! My first ever project and incredibly fun to build.
+</p>
+<a href="https://github.com/ketka82uk/Tic-Tac-Dino/blob/master/README.md" target="_blank" rel="noreferrer">READ MORE |</a>
+<a href="https://ketka82uk.github.io/Tic-Tac-Dino/" target="_blank" rel="noreferrer"> PlAY TIC-TAC-DINO!</a>
+  </div>
+}
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,31 +1,64 @@
 import React, { useState } from "react"
 import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button"
+import FadeIn from 'react-fade-in'
+import ProjectOne from "../components/ProjectOne"
 import background from "../images/portfolio-background.jpg"
+import tic_tac_dino from "../images/Tic-Tac-Dino-high.gif"
+import etc_news from "../images/ETC.gif"
+import tv from "../images/TV.png"
 
 
 export default function Portfolio() {
 
   const [tvStatic, setTvStatic] = useState(true)
   const [projectOne, setProjectOne] = useState(false)
+  const [projectTwo, setProjectTwo] = useState(false)
+
 
   const toggleProjectOne = () => {
     setProjectOne(!projectOne)
-    setTvStatic(!tvStatic)
-    console.log('look here!', tvStatic)
+    setTvStatic(false)
+    setProjectTwo(false)
+  }
+
+  const toggleProjectTwo = () => {
+    setProjectOne(false)
+    setTvStatic(false)
+    setProjectTwo(!projectTwo)
   }
 
   return (
     <div className="page-contents-center" style={{ backgroundImage: `url(${background})`, backgroundSize: '100% 100%' }}>
       <Container>
-        <div className="tv-container">
-          <div className="outer-tv-container">
-            {tvStatic && <div className="inner-tv-container"></div>}
-            {projectOne && <div className="project-one"></div>}
-            <div className="tv"></div>
+        <div className="portfolio-container">
+          <div className="portfolio-upper-section">
+          <div className="portfolio-header-section">
+            <div className="portfolio-header"><h2>portfolio</h2></div>
+            </div>
+            <div className="portfolio-description-section">
+            {projectOne && <div className="portfolio-description"><FadeIn><ProjectOne /></FadeIn></div>}
+            </div>
           </div>
-          <Button onClick={toggleProjectOne}>Channel 1</Button>
-        </div> 
+          <div className="tv-container">
+            <div className="outer-tv-container">
+              {tvStatic && <div className="inner-tv-container"></div>}
+              {projectOne && <div className="project-one" style={{ backgroundImage: `url(${tic_tac_dino})` }}></div>}
+              {projectTwo && <div className="project-one" style={{ backgroundImage: `url(${etc_news})` }}></div>}
+              <div className="tv">
+                <div className="button-overlay">
+                  <div className="button-container">
+                    <Button className="channel-button" onClick={toggleProjectOne}>Project 1</Button>
+                    <Button className="channel-button" onClick={toggleProjectTwo}>Project 2</Button>
+                    <Button className="channel-button" onClick={toggleProjectOne}>Project 3</Button>
+                    <Button className="channel-button" onClick={toggleProjectOne}>Project 4</Button>
+                  </div>
+                </div>
+                <img src={tv} alt="retro TV" height="480px"></img>
+              </div>
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   )
