a1991b1477da817f61219a7f3c1c84920b2b4a38
Cathy <cathy_thomas_uk@yahoo.com>
2021-04-06 21:06:18 +0100

    6Apr2106

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,19 @@
 module.exports = {
   /* Your site config here */
   plugins: [`gatsby-plugin-sass`], 
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-antd",
+      options: {
+        style: true,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-less",
+      options: {
+        javascriptEnabled: true,
+      },
+    }
+  ]
 }
